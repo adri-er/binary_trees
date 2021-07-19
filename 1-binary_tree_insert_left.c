55b24e778ba8c52261c5ea0d94ccca03cb576293
lepc1972 <githublepc@gmail.com>
2021-07-19 22:23:05 +0000

    1-pendiente

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * @brief
+ *
+ * @param parent
+ * @param value
+ * @return binary_tree_t*
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+
+    if (parent == NULL)
+        return NULL;
+
+    new = binary_tree_node(parent, value);
+    if (new == NULL)
+        return NULL;
+    if (parent->left != NULL)
+        new->left = parent->left;
+
+
+
+
+
+    parent->left = new;
+    return(new);
+}
