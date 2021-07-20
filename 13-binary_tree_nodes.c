0e598b515ba31c052e2e09c4f3908469e0a673a8
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 12:40:14 -0500

    [FEATURE] (Determine num of nodes with childs)

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,12 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param tree 
- * @return size_t 
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree.
+ * @tree: pointer to the root node of the tree to count the number of nodes.
+ *
+ * Return: 0 if tree is null, otherwise the nodes with child.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+    size_t childs = 0;
 
+    if (tree)
+    {
+        if (tree->left || tree->right)
+            childs += 1;
+        childs += binary_tree_nodes(tree->left);
+        childs += binary_tree_nodes(tree->right);
+    }
+    return (childs);
 }
