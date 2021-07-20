f871c49eaf6994f8a5d3fd5064fe6bfaa4d2f08d
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 16:18:15 -0500

    [FEATURE] (Determine balance facato bin tree)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param tree 
- * @return int 
+ * binary_tree_balance - measures the balance factor of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: 0 if tree is NULL, otherwise the balance factor. 
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    size_t height_l;
+	size_t height_r;
 
-}
+    if (tree->left)
+        height_l = 1 + binary_tree_balance(tree->left);
+    else
+        height_l = 0;
+
+    if (tree->right)
+        height_r = 1 + binary_tree_balance(tree->right);
+    else
+        height_r = 0;
+    
+    return (height_l - height_r);
+ }
