6061cc5dad1deb96c9aa5d2add49cc42c7ec905e
lepc1972 <githublepc@gmail.com>
2021-07-20 09:06:33 -0500

    del ready

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,11 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param tree 
+ * binary_tree_delete - deletes a binary tree.
+ * @tree: pointer to the root of the tree to delete.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+    if (tree != NULL)
+    {
+        binary_tree_delete(tree->right);
+        binary_tree_delete(tree->left);
+        free(tree);
+    }
+
 
 }
