f15bc708250e4554cb2e867e014890a3ff8bb648
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 17:58:58 -0500

    [FEATURE] (finds the uncle of a node)

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node.
+ * @node: pointer to the node to find the sibling.
+ *
+ * Return: a pointer to the sibling node.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    binary_tree_t *sibling;
+
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
+
+    if (node->parent->left == node)
+        sibling = node->parent->right;
+    else
+        sibling = node->parent->left;
+
+    return (sibling);
+}
+
+/**
+ * binary_tree_uncle - finds the uncle of a node.
+ * @node: pointer to the node to find the uncle.
+ *
+ * Return: NULL if node is NULL, there is no uncle, otherwise return uncle.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
+
+    return (binary_tree_sibling(node->parent));
+}
