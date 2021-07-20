08c9e53e800b5bb906a3768246c9818eb6617ff6
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 09:26:27 -0500

    [FEATURE] (Check if node is a leaf)

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,6 +3,8 @@
 /**
  * binary_tree_delete - deletes a binary tree.
  * @tree: pointer to the root of the tree to delete.
+ * 
+ * Return: None.
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
@@ -12,6 +14,4 @@ void binary_tree_delete(binary_tree_t *tree)
         binary_tree_delete(tree->left);
         free(tree);
     }
-
-
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,12 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param node 
- * @return int 
+ * binary_tree_is_leaf - checks if a node is a leaf.
+ *
+ * @node: node to check.
+ * Return: 1 if is a leaf, 0 otherwise.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+    if (node == NULL || node->left != NULL || node->right != NULL)
+        return (0);
 
+    return (1);
 }
