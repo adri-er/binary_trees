aeff5f6cdfbff1913e28cf0adf1d4b5438fac417
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 11:43:14 -0500

    [FEATURE] (Count the amount of leafs binary tree)

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param tree 
- * @return size_t 
+ * binary_tree_leaves - measures the size of a binary tree.
+ * @tree: pointer to the root node of the tree to count the number of leaves.
+ *
+ * Return: 0 if tree is NULL, size of the tree otherwise.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+    size_t leaves = 0;
 
+    if (tree)
+    {
+        if (!tree->left && !tree->right)
+            leaves += 1;
+        leaves += binary_tree_leaves(tree->left);
+        leaves += binary_tree_leaves(tree->right);
+    }
+    return (leaves);
 }
