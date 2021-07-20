60a1829e0c10b8feeb0cdbe069c4dd142fca17f9
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 17:51:43 -0500

    [FEATURE] (Return pointer to the sibling of a nod)

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param node 
- * @return binary_tree_t* 
+ * binary_tree_sibling - finds the sibling of a node.
+ * @node: pointer to the node to find the sibling.
+ *
+ * Return: a pointer to the sibling node.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+    binary_tree_t *sibling;
 
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
+
+    if (node->parent->left == node)
+        sibling = node->parent->right;
+    else
+        sibling = node->parent->left;
+
+    return (sibling);
 }
