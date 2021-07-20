d8692fd0cbea9014c0908a44cfb52b3b0d72e6dd
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 18:02:42 -0500

    [STYLE] (Remove tags in the header file)

--- binary_trees.h
+++ binary_trees.h
@@ -5,10 +5,6 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-
-/* -------------- MACROS ------------------ */
-
-
 /* ------------- STRUCTURES --------------- */
 
 /**
@@ -35,7 +31,6 @@ typedef struct binary_tree_s heap_t;
 
 /* ------------ PROTOTYPES ---------------- */
 /* ---- binary_print.c ---- */
-
 void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
