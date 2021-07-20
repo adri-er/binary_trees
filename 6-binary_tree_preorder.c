3a4c248410f25a5b9665523a685fe383aa3c3a7a
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-20 10:09:33 -0500

    [FEATURE] (Go through tree in pre-traversal order)

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,12 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
- * 
- * @param tree 
- * @param func 
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal.
+ * @tree: pointer to the root node of the tree to traverse.
+ * @func: pointer to a function to call for each node.
+ *
+ * Return: None.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (tree && func)
+    {
+        func(tree->n);
+        binary_tree_preorder(tree->left, func);
+        binary_tree_preorder(tree->right, func);
+    }
 }
