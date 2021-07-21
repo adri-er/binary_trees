bdcb89554057e42212fa8d0a7464185328a4bbdd
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-21 14:16:12 -0500

    [FIX] (consider null case)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,18 +11,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l;
 	size_t height_r;
 
-	if (tree->left)
-		height_l = 1 + binary_tree_height(tree->left);
-	else
-		height_l = 0;
+	if (tree)
+	{
+		if (tree->left)
+			height_l = 1 + binary_tree_height(tree->left);
+		else
+			height_l = 0;
 
-	if (tree->right)
-		height_r = 1 + binary_tree_height(tree->right);
-	else
-		height_r = 0;
+		if (tree->right)
+			height_r = 1 + binary_tree_height(tree->right);
+		else
+			height_r = 0;
 
-	if (height_l > height_r)
-		return (height_l);
-	else
-		return (height_r);
+		if (height_l > height_r)
+			return (height_l);
+		else
+			return (height_r);
+	}
+	return (0);
 }
