eb72097a0077938755f5ceed65ea1d8696eb9154
Adriana Echeverri <adriana.er98@hotmail.com>
2021-07-21 14:24:03 -0500

    [FIX] (Refactor the internal variables)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,18 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t height_l;
-	size_t height_r;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
 	if (tree->left)
-		height_l = 1 + binary_tree_balance(tree->left);
+		height_l += 1 + binary_tree_balance(tree->left);
 	else
 		height_l = 0;
 
 	if (tree->right)
-		height_r = 1 + binary_tree_balance(tree->right);
+		height_r += 1 + binary_tree_balance(tree->right);
 	else
 		height_r = 0;
 
-	return (height_r - height_l);
+	return (height_l - height_r);
 }
